c447e82fd14eee8b30e6194b7aaf599c166f2550
Valentyna Paniuk <Paniuk@rivne.itstep.org>
2020-02-13 13:49:33 +0200

    Singleton

--- Classwork20200212/Classwork20200212/Source20200212.cpp
+++ Classwork20200212/Classwork20200212/Source20200212.cpp
@@ -1,59 +1,59 @@
-﻿#include<iostream>
-#include<string>
+﻿#include <iostream>
+#include <string>
+
 using namespace std;
 
+/*1. Написати клас Logger в якому можна залогінитись лише один раз. (Singleton) 
+у випадку якщо екземпляр класу вже створений, повертати відповідне повідомлення*/
+
+
 class Logger {
-	string username;
+	string name;
 
 	//статитчне поле контролю кількості об'єктів
-	static Logger * logger_;
+	static Logger * logger;
 
 	//делегуючий конструктор
-	Logger(string username) : username(username) {};
+	Logger(string username) : name(name) {};
 
 	//конструктор копіювання
 	Logger(const Logger&) = delete;
-	
+
 	//приховані методи копіювання через оператор()
 	Logger operator =(Logger&) = delete;
 public:
 
 	//Статичний метод, що повертає попереднє значення користувача
-	static Logger *GetInstance(string username) {
-		if (logger_ == nullptr) {
-			logger_ = new Logger(username);
+	static Logger *GetInstance(string name) {
+		if (logger == nullptr) {
+			logger = new Logger(name);
 			cout << "Logined" << endl;
 		}
 		else {
 			cout << "User already loggined." << endl;
 		}
-		return logger_;
+		return logger;
 	}
 
 	//Метод виводу
 	void ShowInfo() {
-		cout << "Name: " << username << endl;
+		cout << "Name: " << name << endl;
 	}
 };
 
-Logger * Logger::logger_ = nullptr; //ініціалізація статичного поля
+Logger * Logger::logger = nullptr; //ініціалізація статичного поля
 
-int main() {
+int main()
+{
+	string name;
+	cout << "Enter username: ";
+	cin >> name;
+	Logger::GetInstance(name)->ShowInfo();
+	cout << "Enter username: ";
+	cin >> name;
+	Logger::GetInstance(name)->ShowInfo();
 
-	
-	string username;
-	cout << "Enter username: ";
-	cin >> username;
-	Logger::GetInstance(username)->ShowInfo();
-	cout << "Enter username: ";
-	cin >> username;
-	Logger::GetInstance(username)->ShowInfo();
 
 	system("pause");
-
-
-
-
-
 	return 0;
 }
